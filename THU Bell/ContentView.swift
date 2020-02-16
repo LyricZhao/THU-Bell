012cf08ec4541b680f80e055b0c999d5ef8dec4e
LyricZhao <lyricz@yeah.net>
2020-02-16 17:51:13 +0800

    Initial Commit

---
+++ THU Bell/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  THU Bell
+//
+//  Created by Lyric Zhao on 2020/2/16.
+//  Copyright © 2020 Lyric Zhao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
